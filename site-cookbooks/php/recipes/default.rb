813fa7f1499fdc0254675ad05a632dfcdf6b4701
kamakura-masaya <kamakura.masaya@gmail.com>
2013-12-31 02:30:29 +0900

    add php5 recipe

---
+++ site-cookbooks/php/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: php
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'php5' do
+	action :install
+	version "5.3.10-1ubuntu3.9"
+end
