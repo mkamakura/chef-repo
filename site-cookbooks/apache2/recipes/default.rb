9181b9415a971ab986ca412d058c8836073c40d8
kamakura-masaya <kamakura.masaya@gmail.com>
2013-12-31 01:11:55 +0900

    create apache2 install recipe

---
+++ site-cookbooks/apache2/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: apache2
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'apache2' do
+	action :install
+end
+
