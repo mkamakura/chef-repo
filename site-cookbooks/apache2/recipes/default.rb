ef5f2f10fa267d2521c36372e17bb5e8919078e4
kamakura-masaya <kamakura.masaya@gmail.com>
2013-12-31 01:45:50 +0900

    add apache2 service

--- site-cookbooks/apache2/recipes/default.rb
+++ site-cookbooks/apache2/recipes/default.rb
@@ -12,9 +12,17 @@ package 'apache2' do
 	version "2.2.22-1ubuntu1.4"
 end
 
+service "apache2" do
+	supports :status=>true, :restart=>true, :reload=>true
+	action [:enable, :start]
+end
+
 template "/etc/apache2/sites-available/default" do
 	source "default"
 	owner "root"
 	group "root"
 	mode 0644
+	notifies :reload, 'service[apache2]'
 end
+
+
